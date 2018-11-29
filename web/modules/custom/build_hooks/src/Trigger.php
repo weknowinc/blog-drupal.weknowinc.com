32ba4d2a2a57adcace5721929a2abd26b34f53df
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2018-11-29 14:28:42 -0800

    fix: update build_hooks module (#14)

--- web/modules/custom/build_hooks/src/Trigger.php
+++ web/modules/custom/build_hooks/src/Trigger.php
@@ -3,7 +3,12 @@
 namespace Drupal\build_hooks;
 
 use Drupal\Core\Config\ConfigFactoryInterface;
-use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use Drupal\Core\Session\AccountProxyInterface;
+use Drupal\Core\Messenger\MessengerInterface;
+use GuzzleHttp\ClientInterface;
+use Drupal\Core\StringTranslation\TranslationManager;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 
 /**
  * Class Trigger.
@@ -11,34 +16,166 @@ use GuzzleHttp\Client;
 class Trigger implements TriggerInterface {
 
   /**
-   * Drupal\Core\Config\ConfigFactoryInterface definition.
+   * The config.factory service
    *
    * @var \Drupal\Core\Config\ConfigFactoryInterface
    */
   protected $configFactory;
 
   /**
-   * GuzzleHttp\ClientInterface definition.
+   * The http_client service
    *
-   * @var \GuzzleHttp\Client
+   * @var \GuzzleHttp\ClientInterface
    */
   protected $httpClient;
+
+  /**
+   * The current_user service
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $currentUser;
+
+  /**
+   * The string_translation service
+   *
+   * @var \Drupal\Core\StringTranslation\TranslationManager
+   */
+  protected $stringTranslation;
+
+  /**
+   * The messenger service
+   *
+   * @var \Drupal\Core\Messenger\MessengerInterface
+   */
+  protected $messenger;
+
+  /**
+   * The logger.factory service
+   *
+   * @var \Drupal\Core\Logger\LoggerChannelFactoryInterface
+   */
+  protected $logger;
 
   /**
    * Constructs a new Trigger object.
    */
   public function __construct(
-    ConfigFactoryInterface $config_factory,
-    Client $http_client
+    ConfigFactoryInterface $configFactory,
+    ClientInterface $httpClient,
+    AccountProxyInterface $currentUser,
+    TranslationManager $stringTranslation,
+    MessengerInterface $messenger,
+    LoggerChannelFactoryInterface $logger
   ) {
-    $this->configFactory = $config_factory;
-    $this->httpClient = $http_client;
+    $this->configFactory = $configFactory;
+    $this->httpClient = $httpClient;
+    $this->currentUser = $currentUser;
+    $this->stringTranslation = $stringTranslation;
+    $this->messenger = $messenger;
+    $this->logger = $logger;
   }
 
   public function execute() {
-    $hook = $this->configFactory
-      ->get('build_hooks.settings')
-      ->get('build_hook');
-    $this->httpClient->post($hook);
+    if (!$this->isValidUser()) {
+      $message = $this->stringTranslation->translate(
+        'Insufficient user acess',
+        []
+      );
+      $this->logger->get('build_hooks')->error($message);
+      return FALSE;
+    }
+
+    try {
+      $hook = $this->configFactory
+        ->get('build_hooks.settings')
+        ->get('build_hook');
+
+       $this->httpClient->post($hook.'-lorem');
+
+       $message = 'Build Hook Executed';
+       $this->processMessage($message);
+       $this->processLog($message);
+    }
+    catch (RequestException $exception) {
+      $message = $this->stringTranslation->translate(
+        'Build Hook execute error: "%error"',
+        ['%error' => $exception->getMessage()]
+      );
+      $this->messenger->addError($message);
+      $this->logger->get('build_hooks')->error($message);
+      return FALSE;
+    }
+
+    return TRUE;
   }
+
+  public function executeCron() {
+    $execute = (bool)$this->configFactory
+      ->get('build_hooks.settings')
+      ->get('triggers.cron');
+
+    if ($execute) {
+      $this->execute();
+    }
+  }
+
+  public function executeNode($nodeType) {
+    $execute = (bool)$this->configFactory
+      ->get('build_hooks.settings')
+      ->get('triggers.node.'.$nodeType);
+
+    if ($execute) {
+      $this->execute();
+    }
+  }
+
+  public function showMenu() {
+    if (!$this->isValidUser()) {
+      return FALSE;
+    }
+
+    return (bool)$this->configFactory
+      ->get('build_hooks.settings')
+      ->get('triggers.menu');
+  }
+
+  private function isValidUser() {
+    return $this->currentUser->hasPermission('access admin');
+  }
+
+  private function processMessage($message, $args = []) {
+    $show = (bool)$this->configFactory
+      ->get('build_hooks.settings')
+      ->get('messages.show');
+
+    if (!$show) {
+      return;
+    }
+
+    $this->messenger->addMessage(
+      $this->stringTranslation->translate(
+        $message,
+        $args
+      )
+    );
+  }
+
+  private function processLog($message, $args = []) {
+    $log = (bool)$this->configFactory
+      ->get('build_hooks.settings')
+      ->get('messages.log');
+
+    if (!$log) {
+      return;
+    }
+
+    $this->logger->get('build_hooks')->info(
+      $this->stringTranslation->translate(
+        $message,
+        $args
+      )
+    );
+  }
+
 }
