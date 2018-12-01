7ea51142d626daac4f51fa663517d5f33a90148b
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2018-12-01 08:50:27 -0800

    fix use tui_editor dependencies as external libraries

--- web/modules/custom/tui_editor/assets/tui_editor.js
+++ web/modules/custom/tui_editor/assets/tui_editor.js
@@ -1,6 +1,6 @@
 /**
  * @file
- * SimpleMDE implementation of {@link Drupal.editors} API.
+ * TUI Editor implementation.
  */
 
 (function(Drupal) {
@@ -38,7 +38,7 @@
                 el: document.querySelector('#editSection'),
                 initialEditType: 'wysiwyg',
                 previewStyle: 'vertical',
-                height: '300px',
+                height: '480px',
                 exts: ['scrollSync'],
                 initialValue: document.getElementById(element.id).value,
                 events: {
@@ -58,7 +58,6 @@
                     'divider',
                     'ul',
                     'ol',
-                    // 'task',
                     'indent',
                     'outdent',
                     'divider',
@@ -72,58 +71,28 @@
             });
 
             /* Override PopupImage Modal */
+            jQuery('#editSection .tui-popup-wrapper').css('index', '1000');
             jQuery('#editSection .te-tab-section').remove();
             jQuery('#editSection .te-file-type').remove();
             jQuery('#editSection .te-url-type').addClass('te-tab-active');
-            jQuery('#editSection .te-image-url-input').hide();
+            jQuery('#editSection .te-image-url-input').prop('readonly', true);
+            jQuery('#editSection .te-image-url-input').attr("placeholder", "Click to open media browser");
 
-            /* Add Image Selector */
-            jQuery('#editSection .te-url-type').append(
-                '<div id="api-file-select"></div>'
-            );
+            jQuery('#editSection .te-image-url-input').on('click', function(e){
+                jQuery('#edit-media-library-open-button').click();
+            });
 
-            /* Fetch Images from Drupal API */
-            var fileItems = [];
-            jQuery.get('/api/media/image?include=field_media_image&page[limit]=1000', function( data ) {
-                
-                var files = data.included;
-                data.data.forEach(function(item) {
-
-                    // Find media-image => file-file relation
-                    var file = jQuery.grep(files, function( element ) {
-                        return element.id === item.relationships.field_media_image.data.id;
-                    });
-
-                    if (file !== undefined) {
-                        fileItems.push({
-                            id: item.id,
-                            text: item.attributes.name,
-                            url: file[0].attributes.url
-                        });
+            jQuery('#edit-media-library-update-widget').on("mousedown",function(e){
+                var mediaId = jQuery('#edit-media-library-selection').val();
+                jQuery.get('/api/media/image?filter[mid][value]='+mediaId+'&include=field_media_image', function( data ) {
+                    if (data.included) {
+                        jQuery('#editSection .te-image-url-input').val( data.included[0].attributes.url );
+                        jQuery('#editSection .te-alt-text-input').val( data.included[0].attributes.filename );
+                        jQuery('#editSection .te-alt-text-input').focus();
                     }
                 });
 
-                /* Format Image Selector Item */
-                function fileItemFormater(file) {
-                    var $fileTemplate = jQuery('<span class="select-file-item"><img width="90" heighy="60" src="'+file.url+'" />'+file.text+'</span>');
-                    return $fileTemplate;
-                }
-
-                /* Append images to Image Selector */
-                jQuery('#api-file-select').select2({
-                    placeholder: 'Select Image',
-                    width: '100%',
-                    data: fileItems,
-                    templateResult: fileItemFormater,
-                    selectOnClose: true,
-                });
-
-                jQuery('#api-file-select').on('select2:select', function (e) {
-                    var data = e.params.data;
-                    jQuery('#editSection .te-image-url-input').val(data.url);
-                    jQuery('#select2-api-file-select-container').html(data.text);
-                    jQuery('#select2-api-file-select-container').attr('title', data.text);
-                });
+                return false;
             });
 
             return editor;
