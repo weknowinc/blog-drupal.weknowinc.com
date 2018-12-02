84b88bf357a46292326092a20e6850ea186f8d93
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2018-12-02 11:34:32 -0800

    fix tui_editor

--- web/modules/custom/tui_editor/assets/tui_editor.js
+++ web/modules/custom/tui_editor/assets/tui_editor.js
@@ -24,15 +24,18 @@
          *   Whether the editor was successfully attached.
          */
         attach: function(element, format) {
-            var textarea = document.getElementById(element.id);
-            var textarea_wapper = textarea.parentNode;
-            textarea_wapper.id = element.id + '-wrapper';
+            jQuery('#edit-body-wrapper').hide();;
+            var textarea_wapper_id = 'node-edit-form-textarea-wrapper';
+            jQuery('#edit-body-wrapper').wrapAll('<div id="'+textarea_wapper_id+'"></div>');
 
-            var textarea_field_wrapper = document.getElementById('node-edit-form-textarea-wrapper');
-            textarea_field_wrapper.innerHTML += '<div class="code-html" style="border: none;"><div id="editSection"></div></div>';
+            // @TODO Standardize with jQuery methods
+            document.getElementById(textarea_wapper_id).innerHTML += '<div class="code-html" style="border: none;"><div id="editSection"></div></div>';
+            document.getElementById(textarea_wapper_id).innerHTML += '<a href="/admin/content/media-widget?media_library_widget_id=markdown_media&amp;media_library_allowed_types%5B0%5D=image&amp;media_library_remaining=1" class="btn button use-ajax media-library-open-button hidden" data-dialog-type="modal" data-dialog-options="{&quot;dialogClass&quot;:&quot;media-library-widget-modal&quot;,&quot;height&quot;:&quot;75%&quot;,&quot;width&quot;:&quot;75%&quot;,&quot;title&quot;:&quot;Media library&quot;}" data-drupal-selector="edit-media-library-open-button" id="edit-media-library-open-button" style="display: none;">Browse media</a>';
+            document.getElementById(textarea_wapper_id).innerHTML += '<input data-media-library-widget-value="markdown_media" data-drupal-selector="edit-media-library-selection" type="text" id="edit-media-library-selection" name="media_library_selection" value="" size="60" maxlength="128" class="form-text hidden" style="display: none;">';
+            document.getElementById(textarea_wapper_id).innerHTML += '<input data-media-library-widget-update="markdown_media" onsubmit="return false;" data-drupal-selector="edit-media-library-update-widget" type="submit" id="edit-media-library-update-widget" name="media-library-update" value="Update widget" class="button js-form-submit form-submit hidden" style="display: none;">';
 
             // @TODO strip HTML tags (br,other?)
-            jQuery('#'+textarea_wapper.id).parents('.field--type-text-with-summary').hide();
+            jQuery('#'+textarea_wapper_id).parents('.field--type-text-with-summary').hide();
 
             var editor = new tui.Editor({
                 el: document.querySelector('#editSection'),
@@ -86,8 +89,8 @@
                 var mediaId = jQuery('#edit-media-library-selection').val();
                 jQuery.get('/api/media/image?filter[mid][value]='+mediaId+'&include=field_media_image', function( data ) {
                     if (data.included) {
-                        jQuery('#editSection .te-image-url-input').val( data.included[0].attributes.url );
-                        jQuery('#editSection .te-alt-text-input').val( data.included[0].attributes.filename );
+                        jQuery('#editSection .te-image-url-input').val(data.included[0].attributes.url);
+                        jQuery('#editSection .te-alt-text-input').val(data.included[0].attributes.filename);
                         jQuery('#editSection .te-alt-text-input').focus();
                     }
                 });
